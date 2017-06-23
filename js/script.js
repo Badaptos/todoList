58516a8e6e5182660e51a1b06e46b5ae382be688
Badaptos <alex.g9415@gmail.com>
2017-06-23 10:12:17 -0700

    js file now adds element to todoList

--- js/script.js
+++ js/script.js
@@ -6,12 +6,31 @@ addTodo.addEventListener('click', function(){
   userInput = document.querySelector('#todoValue').value;
   inputText = document.createTextNode(userInput);
   newTodo = document.createElement('li');
-  buttonWrapper = document.createElement('div').classList.add('actionButtons');
-  buttons = document.createElement('button');
-  // trashIcon = document.createElement('i').classList.add('fa fa-trash');
-  // checkIcon = document.createElement('i').classList.add('fa fa-check');
-  // CREATE ALL ELEMENTS WHICH WILL BE NESTED WITHIN THE LI ELEMENT
-  newTodo.appendChild(inputText);
-  todoList.appendChild(newTodo);
+  buttonWrapper = document.createElement('div');
+  trashButton = document.createElement('button');
+  checkButton = document.createElement('button')
+  trashIcon = document.createElement('i');
+  checkIcon = document.createElement('i');
+
+//check for user input
+  if(userInput === ''){
+    return false;
+  }
+
+//ADD CLASS NAME TO I ELEMENT FOR FONT AWESOME ICONS
+  buttonWrapper.className = 'actionButtons';
+  trashIcon.className = 'fa fa-trash';
+  checkIcon.className = 'fa fa-check';
+
+//Append Elements
+  checkButton.appendChild(checkIcon); //add checkmark icon to button
+  trashButton.appendChild(trashIcon); //add tash icon to button
+  buttonWrapper.appendChild(trashButton); //add trash button to button wrapper
+  buttonWrapper.appendChild(checkButton); // add check button to button wrapper
+
+
+  newTodo.appendChild(inputText); // ADDS THE USER INPUT AS TEXT INTO LI ELEMENT
+  newTodo.appendChild(buttonWrapper);
+  todoList.appendChild(newTodo); //ADDS LI TO todoList UL
 
 });
