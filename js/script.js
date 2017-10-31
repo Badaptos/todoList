39b5e1bf943e45e116e690f7f1d648632b107621
Badaptos <alex.g9415@gmail.com>
2017-10-31 10:36:39 -0700

    Beautified

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //ADD A NEW ITEM
 document.querySelector('.todoButton').addEventListener('click', createTodo);
-document.querySelector('form').addEventListener('submit', function(e){
-  e.preventDefault();
-  createTodo();
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    createTodo();
 });
 // var todos = document.querySelector('.todoWrapper');
 var currentList = document.querySelector('#todos')
@@ -10,92 +10,88 @@ var completedTodos = document.querySelector('#completedTodos'); //CREATE VAR FOR
 completedTodos.style.display = 'none'; //SET
 
 
-function createTodo(){
-  document.querySelector('.actionPrompt').style.display = 'none';
-  var todoList = document.querySelector('#todoList'), //CONTAINER ELEMENT
-  userInput = document.querySelector('#todoValue'), // MOVED .VALUE METHOD FROM USERINPUT VARIABLE AND JUST USE IT WHERE VALUE IS NEEDED. THIS ALLOWS FOR RESETTING INPUT VALUE
+function createTodo() {
+    document.querySelector('.actionPrompt').style.display = 'none';
+    var todoList = document.querySelector('#todoList'), //CONTAINER ELEMENT
+        userInput = document.querySelector('#todoValue'), // MOVED .VALUE METHOD FROM USERINPUT VARIABLE AND JUST USE IT WHERE VALUE IS NEEDED. THIS ALLOWS FOR RESETTING INPUT VALUE
 
-  //CREATE ELEMENTS
-  inputText = document.createTextNode(userInput.value),
-  newTodo = document.createElement('li'),
-  buttonWrapper = document.createElement('div'),
-  trashButton = document.createElement('button'),
-  checkButton = document.createElement('button'),
-  trashIcon = document.createElement('i'),
-  checkIcon = document.createElement('i');
+        //CREATE ELEMENTS
+        inputText = document.createTextNode(userInput.value),
+        newTodo = document.createElement('li'),
+        buttonWrapper = document.createElement('div'),
+        trashButton = document.createElement('button'),
+        checkButton = document.createElement('button'),
+        trashIcon = document.createElement('i'),
+        checkIcon = document.createElement('i');
 
-//check for user input
-  if(userInput.value === ''){
-    return false;
-  }
+    //check for user input
+    if (userInput.value === '') {
+        return false;
+    }
 
-//ADD CLASS NAMES TO ELEMENTS FOR STYLING
-  buttonWrapper.className = 'actionButtons';
-  trashIcon.className = 'fa fa-trash';
-  checkIcon.className = 'fa fa-check';
-  trashButton.className = 'delete';
-  checkButton.className = 'completed';
+    //ADD CLASS NAMES TO ELEMENTS FOR STYLING
+    buttonWrapper.className = 'actionButtons';
+    trashIcon.className = 'fa fa-trash';
+    checkIcon.className = 'fa fa-check';
+    trashButton.className = 'delete';
+    checkButton.className = 'completed';
 
-//Append Elements
+    //Append Elements
 
-  trashButton.appendChild(trashIcon); //add trash icon to button
-  buttonWrapper.appendChild(trashButton); //add trash button to button wrapper
+    trashButton.appendChild(trashIcon); //add trash icon to button
+    buttonWrapper.appendChild(trashButton); //add trash button to button wrapper
 
-  trashButton.addEventListener('click', deleteTodo); //ADDS EVENT LISTENER TO DELETE BUTTON
-
-
-  checkButton.appendChild(checkIcon); //add checkmark icon to button
-  buttonWrapper.appendChild(checkButton); // add check button to button wrapper
-  checkButton.addEventListener('click', completeTodo); //ADDS THE EVENT LISTENER FOR THE COMPLETED BUTTON
-
-  newTodo.appendChild(inputText); // ADDS THE USER INPUT AS TEXT INTO LI ELEMENT
-  newTodo.appendChild(buttonWrapper);
-  todoList.insertBefore(newTodo, todoList.firstChild); //ADDS LI to UL. New LI will be added at the top.
-
-  //CLEAR INPUT FIELD
-  userInput.value = '';
-
-  // todos.unshift(newTodo);
-}//END CREATE TODO FUNCTION
+    trashButton.addEventListener('click', deleteTodo); //ADDS EVENT LISTENER TO DELETE BUTTON
 
 
-function completeTodo(){
-  var completedItem = this.parentNode.parentNode;
-  var completedParent = completedItem.parentNode;
-  var itemContent = completedItem.innerHTML;
-  var completedList = document.getElementById('completedList');
-  var newItem = document.createElement('li');
+    checkButton.appendChild(checkIcon); //add checkmark icon to button
+    buttonWrapper.appendChild(checkButton); // add check button to button wrapper
+    checkButton.addEventListener('click', completeTodo); //ADDS THE EVENT LISTENER FOR THE COMPLETED BUTTON
+
+    newTodo.appendChild(inputText); // ADDS THE USER INPUT AS TEXT INTO LI ELEMENT
+    newTodo.appendChild(buttonWrapper);
+    todoList.insertBefore(newTodo, todoList.firstChild); //ADDS LI to UL. New LI will be added at the top.
+
+    //CLEAR INPUT FIELD
+    userInput.value = '';
+
+    // todos.unshift(newTodo);
+} //END CREATE TODO FUNCTION
 
 
-  completedList.insertBefore(newItem, completedList.firstChild);
-  newItem.innerHTML = itemContent;
-  newItem.childNodes[1].childNodes[0].addEventListener('click', deleteTodo);
-  newItem.childNodes[1].childNodes[1].childNodes[0].classList.add('complete');
+function completeTodo() {
+    var completedItem = this.parentNode.parentNode;
+    var completedParent = completedItem.parentNode;
+    var itemContent = completedItem.innerHTML;
+    var completedList = document.getElementById('completedList');
+    var newItem = document.createElement('li');
 
-  //
-  // NEED TO FIGURE OUT HOW TO REMOVE ELEMENT AFTER ANIMATION OCCURS
-  //
 
-  if(completedTodos.style.display = 'block'){
-    return true;
-  }
-  completedTodos.style.display = 'block';
+    completedList.insertBefore(newItem, completedList.firstChild);
+    newItem.innerHTML = itemContent;
+    newItem.childNodes[1].childNodes[0].addEventListener('click', deleteTodo);
+    newItem.childNodes[1].childNodes[1].childNodes[0].classList.add('complete');
+
+    if (completedTodos.style.display = 'block') {
+        return true;
+    }
+    completedTodos.style.display = 'block';
 }
 
-function deleteTodo(){
-  var item = this.parentNode.parentNode; //USING PARENT NODE TO SCALE UP THE DOM TREE TO GET THE LI
-  var parent = item.parentNode; //SCALING UP ONCE THE DOM TREE FROM ITEM TO GET THE UL
-  parent.removeChild(item); //REMOVES THE CHILD OF PARENT NODE (UL) WHICH IS THE ITEM (LI)
+function deleteTodo() {
+    var item = this.parentNode.parentNode; //USING PARENT NODE TO SCALE UP THE DOM TREE TO GET THE LI
+    var parent = item.parentNode; //SCALING UP ONCE THE DOM TREE FROM ITEM TO GET THE UL
+    parent.removeChild(item); //REMOVES THE CHILD OF PARENT NODE (UL) WHICH IS THE ITEM (LI)
 
-  if(completedList.innerHTML === ''){
-    completedTodos.style.display = 'none';
-  } else{
-    return false;
-  }
+    if (completedList.innerHTML === '') {
+        completedTodos.style.display = 'none';
+    } else {
+        return false;
+    }
 
-  if(document.getElementById('todoList').innerHTML === '' && parent.innerHTML === ''){
-    document.querySelector('.actionPrompt').style.display = 'block';
-  }
+    if (document.getElementById('todoList').innerHTML === '' && parent.innerHTML === '') {
+        document.querySelector('.actionPrompt').style.display = 'block';
+    }
 }
 //if delete button is pressed
 //list item will be deleted
